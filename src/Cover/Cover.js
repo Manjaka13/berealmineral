ac838c09ea8a71ef969b01d98987421951a52e18
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-02-24 14:49:56 +0100

    Adjusted cover

--- src/Cover/Cover.js
+++ src/Cover/Cover.js
@@ -1,12 +1,32 @@
 import React from "react";
+import {FontAwesomeIcon as Icon} from "@fortawesome/react-fontawesome";
+import {faPencilAlt} from "@fortawesome/free-solid-svg-icons";
 import "./Cover.scss";
+import logo from "../assets/logo_berealmineral.png";
 
 function Cover() {
 	return (
 		<div className="cover">
 			<div className="flex-container">
 				<div className="box">
+
+					<div className="left">
+						<h1 className="app-title app-title-md title">Acheter des minéraux en France</h1>
+						<div className="separator"></div>
+						<p className="app-text text">Proident tempor proident consequat dolor officia anim enim occaecat duis incididunt fugiat reprehenderit adipisicing aute do sit anim est sunt in. Proident tempor proident consequat dolor officia anim enim occaecat duis incididunt fugiat reprehenderit adipisicing aute do sit anim est sunt in.</p>
+						<a className="app-text book" href="#book" title="Commander votre produit maintenant !">
+							<Icon icon={faPencilAlt} /> Commander
+						</a>
+					</div>
+
+					<div className="right">
+						<div className="right-flex">
+							<img className="logo" src={logo} alt="Logo Berealmineral" />
+						</div>
+					</div>
+
 				</div>
+				<p className="app-text cpy">Bertrand Rigal © 2017.</p>
 			</div>
 		</div>
 	)
