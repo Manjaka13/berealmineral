9f807c5d8bd163c5445bfc17ed28167f944ac962
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-02-24 14:25:30 +0100

    Added base cover

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.scss";
 import Navigation from "../Navigation/Navigation";
+import Cover from "../Cover/Cover";
 import Copyright from "../Copyright/Copyright";
 
 function App() {
 	return (
 		<div className="app">
 			<Navigation />
+			<Cover />
 			<Copyright />
 		</div>
 	)
---
+++ src/Cover/Cover.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cover.scss";
+
+function Cover() {
+	return (
+		<div className="cover">
+			<div className="flex-container">
+				<div className="box">
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Cover;
