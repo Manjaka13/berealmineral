8fdf139df124088ce40fd4611744ac0b3bca6c47
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-02-24 15:12:41 +0100

    Updated about file to section html tag

--- src/About/About.js
+++ src/About/About.js
@@ -3,7 +3,7 @@ import "./About.scss";
 
 function About() {
 	return (
-		<div id="about" className="about">
+		<section id="about" className="about">
 			<h2 className="app-title app-title-sm title1">Votre fournisseur de pierres précieuses,</h2>
 			<div className="separator"></div>
 			<h3 className="app-title app-title-xlg title2">Berealmineral</h3>
@@ -11,7 +11,7 @@ function About() {
 				Vous voulez acheter des minéraux en France? Chez berealmineral, vous avez sonné à la bonne porte. Nous vous proposons un large choix de pierres brutes ou polies. Les pierres d’ornements, labradorite, quartz rose, cornaline... sont disponibles sous forme de galets, sphères, pendentifs et forme libre essentiellement. Nous effectuons également des commandes particulières. Nous sommes spécialisés dans les cristaux de quartz (citrine, fantôme, chlorite...) bruts ou repolis et disposons de nouveautés régulièrement grâce à de précieux partenaires sur les sites d’extractions.
 			</p>
 			<p className="app-text signature">Bonne visite !</p>
-		</div>
+		</section>
 	)
 }
 
